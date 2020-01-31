352b3c62c50eb005528cab97e291c5259756db4c
marcelo sobreira <marcelo15011@gmail.com>
2020-01-31 16:16:44 -0300

    criação do Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Header from "./components/Header";
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Curso</h1>
+      <Header />
     </div>
   );
 }
